d023d442493b46dad4b883bb1ec066e40ee12687
vit73123 <vit73123@gmail.com>
2022-10-02 22:11:45 +0300

    L01 First Methods 00

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,54 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (Resume resume : storage) {
+            if (resume == null) break;
+            resume = null;
+        }
     }
 
     void save(Resume r) {
+        int index = size();
+        if (index < storage.length)
+            storage[index] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (Resume r : storage)
+            if (r.uuid.equals(uuid)) {
+                resume = r;
+                break;
+            }
+        return resume;
     }
 
     void delete(String uuid) {
+        boolean isDeleted = false;
+        int i = 0;
+        while (storage[i] != null) {
+            if (storage[i].uuid.equals(uuid))
+                isDeleted = true;
+            else if (isDeleted)
+                storage[i - 1] = storage[i];
+            i++;
+        }
+        if (isDeleted) storage[i - 1] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int i = 0;
+        for (Resume r : storage) {
+            if (r == null) break;
+            i++;
+        }
+        return i;
     }
 }
