0d64246b1e4432805126384346249bd153dc6443
vit73123 <vit73123@gmail.com>
2023-01-08 17:31:11 +0300

    L06 HW feat: Add getAllExpected in Test #1

--- test/storage/MapUuidStorageTest.java
+++ test/storage/MapUuidStorageTest.java
@@ -1,8 +1,31 @@
 package storage;
 
+import model.Resume;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 class MapUuidStorageTest extends AbstractStorageTest {
 
     public MapUuidStorageTest() {
         super(new MapUuidStorage());
+    }
+
+    protected Resume[] getAllExpected(Storage storage, Resume[] expected) throws NoSuchFieldException, IllegalAccessException {
+        Resume[] storageArray = new Resume[storage.size()];
+        Class<? extends Storage> storageClass = storage.getClass();
+        Field field = storageClass.getDeclaredField("storage");
+        field.setAccessible(true);
+        Map<String, Resume> testStorage = (HashMap) field.get(storage);
+        int i = 0;
+        for(Map.Entry<String, Resume> entry : testStorage.entrySet()) {
+            storageArray[i] = entry.getValue();
+            i++;
+        }
+        field.setAccessible(false);
+
+        return storageArray;
     }
 }
