77b23332d39da2f9629d0f7a43680dd347847689
vit73123 <vit73123@gmail.com>
2023-01-17 11:41:28 +0300

    L06 HW fix: Add AbstractArrayStorageTest

--- test/storage/AbstractArrayStorageTest.java
+++ test/storage/AbstractArrayStorageTest.java
@@ -1,30 +1,11 @@
 package storage;
 
-import exception.StorageException;
-import model.Resume;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+public class AbstractArrayStorageTest extends AbstractStorageTest {
 
-import static storage.AbstractArrayStorage.STORAGE_LIMIT;
+    private final Storage storage;
 
-class AbstractArrayStorageTest extends AbstractStorageTest {
-    protected final Storage storage;
-
-    protected AbstractArrayStorageTest(Storage storage) {
+    AbstractArrayStorageTest(Storage storage) {
         super(storage);
         this.storage = storage;
-    }
-
-    @Test
-    protected void saveOverflow() {
-        try {
-            while (storage.size() < STORAGE_LIMIT) {
-                storage.save(new Resume());
-            }
-        } catch (StorageException e) {
-            Assertions.fail("Failed: Too early overflow");
-        }
-        Assertions.assertThrows(StorageException.class, () ->
-                storage.save(new Resume()));
     }
 }
