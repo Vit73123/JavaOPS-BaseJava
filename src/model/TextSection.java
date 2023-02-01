a97cf285532ee7832acdf6c890d4e6add23f92d8
vit73123 <vit73123@gmail.com>
2023-02-01 11:03:05 +0300

    L07 HW: Domen model Resume

---
+++ src/model/TextSection.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.Objects;
+
+public class TextSection extends Section {
+    private final String content;
+
+    public TextSection(String content) {
+        Objects.requireNonNull(content, "content must nut be null");
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TextSection that = (TextSection) o;
+        return Objects.equals(content, that.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return content != null ? Objects.hash(content) : 0;
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
+}
