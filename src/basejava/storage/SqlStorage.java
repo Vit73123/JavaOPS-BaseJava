1e2ec2fc132e5ebef1dfca331f00b56cc30cb25d
vit73123 <vit73123@gmail.com>
2023-04-12 16:02:17 +0300

    L14: SqlStorage Contacts

--- src/basejava/storage/SqlStorage.java
+++ src/basejava/storage/SqlStorage.java
@@ -2,12 +2,14 @@ package basejava.storage;
 
 import basejava.exception.NotExistStorageException;
 import basejava.exception.StorageException;
+import basejava.model.ContactType;
 import basejava.model.Resume;
 import basejava.sql.SqlHelper;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class SqlStorage implements Storage {
 
@@ -19,71 +21,117 @@ public class SqlStorage implements Storage {
 
     @Override
     public void clear() {
-        sqlHelper.execute("DELETE FROM resume");
+        sqlHelper.execute("""
+            DELETE
+              FROM resume""");
     }
 
     @Override
     public Resume get(String uuid) {
-        return sqlHelper.execute("SELECT * FROM resume r WHERE r.uuid = ?", ps -> {
-            ps.setString(1, uuid);
-            ResultSet rs = ps.executeQuery();
-            if (!rs.next()) {
-                throw new NotExistStorageException(uuid);
-            }
-            return new Resume(uuid, rs.getString("full_name"));
-        });
+        return sqlHelper.execute("""
+            SELECT      *
+              FROM      resume r 
+              left join contact c
+                on      r.uuid = c.resume_uuid  
+              WHERE     r.uuid = ?""",
+            ps -> {
+                ps.setString(1, uuid);
+                ResultSet rs = ps.executeQuery();
+                if (!rs.next()) {
+                    throw new NotExistStorageException(uuid);
+                }
+                Resume r = new Resume(uuid, rs.getString("full_name"));
+                do {
+                    String value = rs.getString("value");
+                    ContactType type = ContactType.valueOf(rs.getString("type"));
+                    r.addContact(type, value);
+                } while (rs.next());
+                return r;
+            });
     }
 
     @Override
     public void update(Resume r) {
-        sqlHelper.<Void>execute("update resume set full_name = ? where uuid = ?", ps -> {
-            ps.setString(1, r.getFullName());
-            ps.setString(2, r.getUuid());
-            if (ps.executeUpdate() == 0) {
-                throw new NotExistStorageException(r.getUuid());
-            }
-            return null;
-        });
+        sqlHelper.<Void>execute("""
+            update  resume
+              set   full_name = ?
+              where uuid = ?""",
+            ps -> {
+                ps.setString(1, r.getFullName());
+                ps.setString(2, r.getUuid());
+                if (ps.executeUpdate() == 0) {
+                    throw new NotExistStorageException(r.getUuid());
+                }
+                return null;
+            });
     }
 
     @Override
     public void save(Resume r) {
-        sqlHelper.<Void>execute("INSERT INTO resume (uuid, full_name) VALUES (?,?)", ps -> {
-            ps.setString(1, r.getUuid());
-            ps.setString(2, r.getFullName());
-            ps.execute();
-            return null;
-        });
+        sqlHelper.<Void>execute("""
+            INSERT
+              INTO   resume (uuid, full_name)
+              VALUES (?, ?)""",
+            ps -> {
+                ps.setString(1, r.getUuid());
+                ps.setString(2, r.getFullName());
+                ps.execute();
+                return null;
+            });
+        for (Map.Entry<ContactType, String> e : r.getContacts().entrySet()) {
+            sqlHelper.<Void>execute("""
+                INSERT
+                  INTO   contact (resume_uuid, type, value)
+                  VALUES (?, ?, ?)""",
+                ps -> {
+                    ps.setString(1, r.getUuid());
+                    ps.setString(2, e.getKey().name());
+                    ps.setString(3, e.getValue());
+                    ps.execute();
+                    return null;
+                });
+        }
     }
 
     @Override
     public void delete(String uuid) {
-        sqlHelper.<Void>execute("delete from resume where uuid = ?", ps -> {
-            ps.setString(1, uuid);
-            if (ps.executeUpdate() == 0) {
-                throw new NotExistStorageException(uuid);
-            }
-            return null;
-        });
+        sqlHelper.<Void>execute("""
+            delete
+              from  resume
+              where uuid = ?""",
+            ps -> {
+                ps.setString(1, uuid);
+                if (ps.executeUpdate() == 0) {
+                    throw new NotExistStorageException(uuid);
+                }
+                return null;
+            });
     }
 
     @Override
     public List<Resume> getAllSorted() {
-        return sqlHelper.execute("SELECT * FROM resume r order by full_name, uuid", ps -> {
-            ResultSet rs = ps.executeQuery();
-            List<Resume> resumes = new ArrayList<>();
-            while (rs.next()) {
-                resumes.add(new Resume(rs.getString("uuid"), rs.getString("full_name")));
-            }
-            return resumes;
-        });
+        return sqlHelper.execute("""
+            SELECT     *
+              FROM     resume r
+              order by full_name, uuid""",
+            ps -> {
+                ResultSet rs = ps.executeQuery();
+                List<Resume> resumes = new ArrayList<>();
+                while (rs.next()) {
+                    resumes.add(new Resume(rs.getString("uuid"), rs.getString("full_name")));
+                }
+                return resumes;
+            });
     }
 
     @Override
     public int size() {
-        return sqlHelper.execute("select count(*) from resume", st -> {
-            ResultSet rs = st.executeQuery();
-            return rs.next() ? rs.getInt(1) : 0;
-        });
+        return sqlHelper.execute("""
+            select count(*)
+              from resume""",
+            ps -> {
+                ResultSet rs = ps.executeQuery();
+                return rs.next() ? rs.getInt(1) : 0;
+            });
     }
 }
